d3107a109c2e020e934cfe13d5be5ed3808e3c81
Susovan Paul <palsusovan88@gmail.com>
2025-02-23 12:01:47 +0530

    image

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,4 +1,7 @@
 import { createContext,useContext, useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import {toast} from "react-toastify";
+import axios from "axios";
 
 
 const UserContext=createContext();
@@ -7,11 +10,12 @@ const UserContextProvider=({children})=>{
 
     const storedToken = localStorage.getItem("uToken") || "";
     const [uToken, setUToken] = useState(storedToken);
-
+    const [user,setUser]=useState({});
+    const navigate=useNavigate();
     const backendUrl=import.meta.env.VITE_BACKEND_URL;
 
     const value={
-        uToken,setUToken,backendUrl,
+        uToken,setUToken,backendUrl,user
     }
 
     useEffect(() => {
@@ -21,6 +25,32 @@ const UserContextProvider=({children})=>{
           localStorage.removeItem("uToken");
         }
       }, [uToken]);
+
+
+      useEffect(()=>{
+        const getUserData=async()=>{
+          try{
+            if(!uToken){
+              toast.error("Not Token found! Please Log in again.");
+              navigate('/login');
+            }
+            else{
+              const {data}=await axios.get(backendUrl+`/api/user/profile`,{headers:{uToken:uToken}});
+              if(!data.success){
+                toast.error("User Not Found. Please Log In Again...");
+                navigate('/login');
+              }
+              else{
+                console.log(data.user);
+                setUser(data.user);
+              }
+            }
+          }catch(err){
+            toast.error(err.message);
+          }
+        }
+        getUserData();
+      },[uToken, backendUrl]);
 
     return (
         <UserContext.Provider value={value}>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,15 @@ import Banner from '../components/Banner'
 function Home() {
   return (
     <div>
+        <div className="emergency-alert mt-6 p-4 bg-red-100 border-l-4 border-red-600 rounded-md mb-9">
+          <h3 className="font-medium text-xl text-red-600">Emergency Alert</h3>
+          <p className="text-sm text-red-700 mt-2">
+            Your oxygen level is risky. Please take immediate action and alert emergency services.
+          </p>
+          <div className="mt-4 flex space-x-4">
+            <button className="p-2 bg-red-600 text-white rounded-md">Send Alert</button>
+          </div>
+        </div>
       <Headers/>
       <SpecialityMeanu/>
       <TopDoctors/>
